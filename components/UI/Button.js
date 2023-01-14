c81a5898360b02b062376d2658581b2144d9afa6
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2023-01-14 13:29:34 +0500

    updated

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -13,7 +13,7 @@ const variants = {
 function Button({ children, className, variant = "normal" }) {
   return (
     <button
-      className={`${className} ${variants[variant].className} py-1 px-4 rounded-full transition-all duration-200`}
+      className={`${className} ${variants[variant].className} py-1 px-4 rounded-full transition-all duration-200 text-xs sm:text-sm lg:text-base`}
     >
       {children}
     </button>
--- components/UI/Navbar.js
+++ components/UI/Navbar.js
@@ -4,8 +4,9 @@ import Button from "./Button";
 function Navbar() {
   return (
     <div className="absolute top-0 left-0 w-full z-[10000]">
-      <div className="container h-[90px] flex items-center justify-between">
-        <img src="images/logo-main.png" className="h-[40px]" alt="" />
+      <div className="container h-[70px] lg:h-[90px] flex items-center justify-between">
+        {/* <img src="images/logo-main.png" className="h-[40px]" alt="" /> */}
+        <h1 className="text-2xl lg:text-3xl font-bold font-myriad">NOVA</h1>
 
         <div className="flex items-center space-x-4">
           <Button>Trade</Button>
